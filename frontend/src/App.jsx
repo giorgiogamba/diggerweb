dd5ffac1a0569d8b7f497a784ad2f198c5e384f9
Giorgio Gamba <giorgiogamba97@gmail.com>
2025-04-14 22:52:27 +0200

    remove method for release component creation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -74,30 +74,6 @@ function App()
     handleSearch(1);
   }
 
-  const ReleaseInfoComponent = ({data}) =>
-  {
-    const headers = Object.keys(data[0]);
-    const rows = data.map(item => Object.values(item));
-
-    return (
-      <table>
-        <thead>
-          <tr>
-            {headers.map(header => <th key={header}>{header}</th>)}
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((row, index) =>
-            (
-              <tr key={index}>
-                {row.map((cell, index) => <td key={index}>{cell}</td>)}
-              </tr>
-            ))}
-        </tbody>
-      </table>
-    );
-  }
-
   return (
     <div className="App">
       <h1>diggerweb</h1>
