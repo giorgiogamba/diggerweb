76ed70167e574ef5c703b8efc2d83bcd66fcdda1
Giorgio Gamba <giorgiogamba97@gmail.com>
2025-04-24 17:21:43 +0200

    define another module path for discogs_api

--- backend/diggerweb_backend/diggerweb_backend/settings.py
+++ backend/diggerweb_backend/diggerweb_backend/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'diggerweb_backend.discogs_api.apps.DiscogsApiConfig',
+	'diggerweb_backend.diggerweb_backend.discogs_api.apps.DiscogsApiConfig',
 	'rest_framework',
 	'corsheaders'
 ]
